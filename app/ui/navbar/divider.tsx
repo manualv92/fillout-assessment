c30d3bbd95a1510c33ef1c1d9f6d16e8aacfbd4f
Manuel Alvarez <manualv92@gmail.com>
2025-06-26 15:07:44 -0300

    hover fix

--- app/ui/navbar/divider.tsx
+++ app/ui/navbar/divider.tsx
@@ -18,11 +18,10 @@ export default function Divider({
       onMouseEnter={() => !isDragging && onHoverChange(true, index)}
       onMouseLeave={() => !isDragging && onHoverChange(false, index)}
       onClick={() => onInsert(index)}
-      className="group relative w-5 h-8 flex items-center justify-center"
+      className="group group-hover:scale-100 relative w-5 h-8 flex items-center justify-center"
     >
       <div className="w-5 border-t-2 border-dashed border-gray-300 absolute z-0" />
       {!isDragging && (
-
         <Icon name="plus-circle" strokeWidth={1.5} className="transition delay-50 duration-100 ease-in opacity-0 scale-0 flex items-center justify-center cursor-pointer z-10 fill-background hover:opacity-100 hover:scale-100" />
       )}
     </div>
