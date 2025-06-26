50ed41a1091b9b11b4fb14b783dc8120801d10e6
Manuel Alvarez <manualv92@gmail.com>
2025-06-26 10:22:58 -0300

    removed font awesome lib

--- app/lib/fontawesome.ts
+++
@@ -1,4 +0,0 @@
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { faInfoCircle, faFlag } from '@fortawesome/free-solid-svg-icons';
-
-// library.add(faInfoCircle, faFlag);
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Navbar from "./ui/navbar";
-import './lib/fontawesome'
 
 export default function Home() {
   return (
